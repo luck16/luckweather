cb3d2d3b276a7777b303e9cca28eab42f0429d6d
luck16 <1101033911@qq.com>
2016-02-26 22:36:25 +0800

    Second commit

---
+++ app/src/androidTest/java/activitytest/example/com/luckweather/util/HttpUtil.java
@@ -0,0 +1,50 @@
+package activitytest.example.com.luckweather.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by wubin on 2016/2/29.
+ */
+public class HttpUtil {
+
+    public static void sendHttpRequest(final String address,final HttpCallbackListener listener){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(adress);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader());
+                    StringBuffer reponse = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        reponse.append(line);
+                    }
+                    if (listener != null) {
+                        //回调onFinish()方法
+                        listener.onFinish(reponse.toString()):
+                    }
+                    catch(Exception e){
+                        if (listener != null){
+                            listener.onError();
+                        }
+                        //回调onError方法
+                    }
+                }finally {
+                    if(connection != null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+}
